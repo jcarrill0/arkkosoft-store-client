b8a7a530c559972f03b43f6b2e1511333c6e214a
Jose Carrillo <josecarrillo8@gmail.com>
2022-09-30 08:48:24 -0600

    Add endpoint production

--- src/api-config/axios.js
+++ src/api-config/axios.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:9000/api/v1/'; 
+const BASE_URL_PROD = 'http://54.164.24.197/api/v1/'; 
+const BASE_URL_DEV = 'http://localhost:9000/api/v1/'; 
 
 export const storeApi = axios.create({
-  baseURL: BASE_URL,
+  baseURL: BASE_URL_PROD,
   headers: {
     'Content-Type': 'application/json'
   }
