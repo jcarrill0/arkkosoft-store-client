07142da184f007d38b22c1156f3b7c5da6379ba4
Jose Carrillo <josecarrillo8@gmail.com>
2022-09-30 17:02:48 -0600

    add new url api rest

--- src/api-config/axios.js
+++ src/api-config/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL_PROD = 'http://54.164.24.197/api/v1/'; 
+const BASE_URL_PROD = 'https://arkkosoft-store.herokuapp.com/api/v1/'; 
 const BASE_URL_DEV = 'http://localhost:9000/api/v1/'; 
 
 export const storeApi = axios.create({
